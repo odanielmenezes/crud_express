86dead2bb48acabcf1c6a97f70099644e3a8fc14
Daniel Menezes <daniel.menezes@infoar.com.br>
2022-04-18 17:19:30 -0300

    FIX: Correção no campo de busca

--- index.js
+++ index.js
@@ -19,37 +19,61 @@ async function getData(users) {
         res.length === 0
           ? "NO DATA"
           : res.map(
-              (item) =>
-                `<div id="data">
+            item =>
+              `<div id="data">
                 <div class="users" id="users">
                   <div id="id">${item.id}</div>
                   <div class="first_name">${item.nome}</div>
                   <div class="last_name">${item.sobrenome}</div>
                 </div>
                 <div id="acoes">
-                <i id="edit-icon" onClick="editUser(${item.id})" class="fas fa-edit"></i>
-                <i id="delete-icon" onClick="openModal(${item.id})" class="fas fa-trash-alt"></i>
+                  <i id="edit-icon" onClick="editUser(${item.id})" class="fas fa-edit"></i>
+                  <i id="delete-icon" onClick="openModal(${item.id})" class="fas fa-trash-alt"></i>
                 </div>
               </div>`
-            ))
+          ).join(''))
     )
   );
   if (users === "NO DATA") {
     document.getElementById("oi").innerHTML =
-      document.getElementById("oi").innerHTML + `<div>${busca}</div>`;
-  } else {
+      document.getElementById("oi").innerHTML + `<div>NO DATA</div>`;
+  }
+  else {
     document.getElementById("oi").innerHTML = users;
   }
+console.log(filmes)
+
 }
 
+
+for (let i = 0; i < document.getElementById('oi').childNodes.length; i++) {
+  if (document.getElementById('oi').childNodes[i].nodeName == "#text") {
+    document.getElementById('oi').childNodes[i].nodeName.toString().replace('')
+  }
+}
 
 input.addEventListener('change', updateValue);
 
 function updateValue(e) {
-  const str = filmes.map(user => user.nome + " " + user.sobrenome)
-  const filtrado = str.filter((user) => user.startsWith(e.target.value))
-  busca.push(filtrado)
-  console.log(busca)
+  var output = filmes.filter(user => user.nome == e.target.value || user.sobrenome == e.target.value || user.id == e.target.value);
+  if (output.length === 0) {
+    openModalBusca(e.target.value)
+  } else {
+    var data = document.getElementById("oi").innerHTML = output.map(user => `<div id="data">
+    <div class="users" id="users">
+    <div id="id">${user.id}</div>
+    <div class="first_name">${user.nome}</div>
+    <div class="last_name">${user.sobrenome}</div>
+    </div>
+    <div id="acoes">
+    <i id="edit-icon" onClick="editUser(${user.id})" class="fas fa-edit"></i>
+    <i id="delete-icon" onClick="openModal(${user.id})" class="fas fa-trash-alt"></i>
+    </div>
+    </div>`
+    ).join('')
+  }
+  console.log(output)
+  busca.push(data)
 }
 
 function onConfirmDelete() {
@@ -57,15 +81,23 @@ function onConfirmDelete() {
   closeModal();
 }
 
+function openModalBusca(busca) {
+  document.getElementById('button-deletar').innerText = "OK"
+  document.querySelector('.none').classList.add('modal-busca')
+  document.getElementById('info-confirm').innerHTML = `<div>Opa! Não encontramos usuários pela busca:<br /><b>${busca}</b></div>`
+}
+
 function openModal(idUser) {
   const user = filmes.find(e => e.id == idUser);
+  document.getElementById('button-deletar').innerText = "DELETAR"
   document.querySelector('.none').classList.add('modal')
   document.getElementById('info-confirm').innerHTML = `<div>Deseja deletar o usuário <br /><b>${user.nome + " " + user.sobrenome}</b> ?</div>`
   index = idUser;
 }
 
 function closeModal() {
-  console.log(document.querySelector('.none').classList.remove('modal'))
+  document.querySelector('.none').classList.remove('modal-busca')
+  document.querySelector('.none').classList.remove('modal')
 }
 
 function deleteUser(idUser) {
@@ -99,43 +131,43 @@ function addUser() {
   ) {
     alert("Preencha os campos do formulário");
   } else {
-  if (method === "POST") {
-    const data = {
-      nome: document.getElementById("nome").value,
-      sobrenome: document.getElementById("sobrenome").value
-    };
-    console.log(data);
-    fetch("http://localhost:3000/post", {
-      method: method,
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        return res.json();
+    if (method === "POST") {
+      const data = {
+        nome: document.getElementById("nome").value,
+        sobrenome: document.getElementById("sobrenome").value
+      };
+      console.log(data);
+      fetch("http://localhost:3000/post", {
+        method: method,
+        headers: {
+          Accept: "application/json text/plain */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       })
-      .catch((err) => console.error("Error", err));
-    attScreen();
-  } else if (method === "PUT") {
-    var data = {
-      nome: document.getElementById("nome").value,
-      sobrenome: document.getElementById("sobrenome").value,
-    };
-    fetch(`http://localhost:3000/put/${index}`, {
-      method: method,
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .catch((err) => console.log(err));
-    attScreen();
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.error("Error", err));
+      attScreen();
+    } else if (method === "PUT") {
+      var data = {
+        nome: document.getElementById("nome").value,
+        sobrenome: document.getElementById("sobrenome").value,
+      };
+      fetch(`http://localhost:3000/put/${index}`, {
+        method: method,
+        headers: {
+          Accept: "application/json text/plain */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+      attScreen();
+    }
   }
-}
   clearFields();
 }
 
